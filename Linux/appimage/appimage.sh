0ee5ff8a6015bd001f06af926440069182dd5b17
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2023-07-05 14:49:46 -0700

    Update appimage.sh

--- Linux/appimage/appimage.sh
+++ Linux/appimage/appimage.sh
@@ -9,8 +9,8 @@ mkdir -p AppDir/usr/bin
 cp bin/linux-x64/Release/linux-x64/publish/Mesen AppDir/usr/bin
 chmod +x AppDir/usr/bin
 
-cp UI/Assets/Mesen.48x48.png AppDir/Mesen.png
-cp UI/Assets/Mesen.desktop AppDir/Mesen.desktop
+cp Linux/appimage/Mesen.48x48.png AppDir/Mesen.png
+cp Linux/appimage/Mesen.desktop AppDir/Mesen.desktop
 mkdir -p AppDir/usr/share/applications && cp ./AppDir/Mesen.desktop ./AppDir/usr/share/applications
 mkdir -p AppDir/usr/share/icons && cp ./AppDir/Mesen.png ./AppDir/usr/share/icons
 mkdir -p AppDir/usr/share/icons/hicolor/48x48/apps && cp ./AppDir/Mesen.png ./AppDir/usr/share/icons/hicolor/48x48/apps
