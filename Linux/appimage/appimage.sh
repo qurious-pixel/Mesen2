4bb8d2b5dced7a26d6dd041fb67204bfa8c06c06
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2023-07-05 14:53:27 -0700

    clang

--- Linux/appimage/appimage.sh
+++ Linux/appimage/appimage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PUBLISHFLAGS="-r linux-x64 --no-self-contained false -p:PublishSingleFile=true -p:PublishReadyToRun=true"
-make -j$(nproc) -O ${{ matrix.use_gcc }} LTO=true STATICLINK=true SYSTEM_LIBEVDEV=false
+make -j$(nproc) -O clang LTO=true STATICLINK=true SYSTEM_LIBEVDEV=false
 
 curl -SL https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
 
