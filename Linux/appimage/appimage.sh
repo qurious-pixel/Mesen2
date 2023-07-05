75495bc4ce752aadd5b81314742bd1c9013f1f1a
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2023-07-05 14:45:36 -0700

    Create appimage.sh

---
+++ Linux/appimage/appimage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export PUBLISHFLAGS="-r linux-x64 --no-self-contained false -p:PublishSingleFile=true -p:PublishReadyToRun=true"
+make -j$(nproc) -O ${{ matrix.use_gcc }} LTO=true STATICLINK=true SYSTEM_LIBEVDEV=false
+
+curl -SL https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
+
+mkdir -p AppDir/usr/bin
+cp bin/linux-x64/Release/linux-x64/publish/Mesen AppDir/usr/bin
+chmod +x AppDir/usr/bin
+
+cp UI/Assets/Mesen.48x48.png AppDir/Mesen.png
+cp UI/Assets/Mesen.desktop AppDir/Mesen.desktop
+mkdir -p AppDir/usr/share/applications && cp ./AppDir/Mesen.desktop ./AppDir/usr/share/applications
+mkdir -p AppDir/usr/share/icons && cp ./AppDir/Mesen.png ./AppDir/usr/share/icons
+mkdir -p AppDir/usr/share/icons/hicolor/48x48/apps && cp ./AppDir/Mesen.png ./AppDir/usr/share/icons/hicolor/48x48/apps
+
+chmod a+x appimagetool
+./appimagetool AppDir/
